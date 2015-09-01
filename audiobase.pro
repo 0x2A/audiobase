c75e8a54b3ab61e361fc88770346e74ced22440a
0x2A <frank.koehnke@fh-heidelberg.de>
2015-09-02 00:55:26 +0200

    Added QT project files

--- audiobase.pro
+++ audiobase.pro
@@ -12,9 +12,9 @@ TARGET = audiobase
 TEMPLATE = app
 
 
-SOURCES += main.cpp\
-        mainwindow.cpp
+SOURCES += src/main.cpp \
+    src/mainwindow.cpp
 
-HEADERS  += mainwindow.h
+HEADERS  += src/mainwindow.h
 
-FORMS    += mainwindow.ui
+FORMS    += src/ui/mainwindow.ui
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QLibraryInfo>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QTranslator qtTranslator;
+    qtTranslator.load("qt_" + QLocale::system().name(),
+                QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    a.installTranslator(&qtTranslator);
+
+    QTranslator myappTranslator;
+        myappTranslator.load("audiobase_" + QLocale::system().name());
+        a.installTranslator(&myappTranslator);
+
     MainWindow w;
     w.show();
 
